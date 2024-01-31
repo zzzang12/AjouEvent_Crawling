394abbedaab0b09ef1bbdbbd8196de0c73e0cced
zzzang12 <wadevs12@gmail.com>
2024-02-01 01:27:42 +0900

    [Refactor] main()에 있던 코드들 함수화

--- main.go
+++ main.go
@@ -3,48 +3,25 @@ package main
 import (
 	. "Notifier/src/notifiers"
 	. "Notifier/src/utils"
-	"context"
-	firebase "firebase.google.com/go"
-	"google.golang.org/api/option"
 	"log"
-	"os"
 	"time"
 )
 
 func main() {
-	CreateDir("./logs")
+	CreateDir("logs")
 
-	errorLog, err := os.OpenFile("logs/errorLog.txt", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0700)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer errorLog.Close()
+	errorLogFile := OpenLogFile("logs/errorLog.txt")
+	defer errorLogFile.Close()
+	ErrorLogger = CreateLogger(errorLogFile)
 
-	sentNoticeLog, err := os.OpenFile("logs/sentNoticeLog.txt", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0700)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer sentNoticeLog.Close()
+	sentNoticeLogFile := OpenLogFile("logs/sentNoticeLog.txt")
+	defer sentNoticeLogFile.Close()
+	SentNoticeLogger = CreateLogger(sentNoticeLogFile)
 
-	ErrorLogger = log.New(errorLog, "", log.Ldate|log.Ltime|log.Lshortfile)
-	SentNoticeLogger = log.New(sentNoticeLog, "", log.Ldate|log.Ltime|log.Lshortfile)
-
-	ctx := context.Background()
-	sa := option.WithCredentialsFile("serviceAccountKey.json")
-	app, err := firebase.NewApp(ctx, nil, sa)
-	if err != nil {
-		ErrorLogger.Fatal(err)
-	}
-	Client, err = app.Firestore(ctx)
-	if err != nil {
-		ErrorLogger.Fatal(err)
-	}
+	Client = ConnectFirebase()
 	defer Client.Close()
 
-	notifierConfigs, err := LoadConfig("NotifierConfigs.json")
-	if err != nil {
-		log.Fatal(err)
-	}
+	notifierConfigs := LoadConfig("notifierConfigs.json")
 
 	notifiers := make([]Notifier, 0, len(notifierConfigs))
 	for _, notifierConfig := range notifierConfigs {
@@ -61,59 +38,6 @@ func main() {
 		}
 		notifiers = append(notifiers, notifier)
 	}
-
-	//notifiers = append(notifiers, AjouNormalNotifier{}.New())
-	//notifiers = append(notifiers, AjouScholarshipNotifier{}.New())
-	//notifiers = append(notifiers, AjouSwNotifier{}.New())
-	//notifiers = append(notifiers, AjouSoftwareNotifier{}.New())
-	//notifiers = append(notifiers, AjouMediaNotifier{}.New())
-	//notifiers = append(notifiers, AjouAAINotifier{}.New())
-	//notifiers = append(notifiers, AjouSecurityNotifier{}.New())
-	//notifiers = append(notifiers, AjouMDCNotifier{}.New())
-	//notifiers = append(notifiers, AjouPharmacyNotifier{}.New())
-	//notifiers = append(notifiers, AjouMedicineNotifier{}.New())
-	//notifiers = append(notifiers, AjouNursingNotifier{}.New())
-	//notifiers = append(notifiers, AjouECENotifier{}.New())
-	//notifiers = append(notifiers, AjouITNotifier{}.New())
-	//notifiers = append(notifiers, AjouAISemiNotifier{}.New())
-	//notifiers = append(notifiers, AjouEngineeringNotifier{}.New())
-	//notifiers = append(notifiers, AjouMENotifier{}.New())
-	//notifiers = append(notifiers, AjouIENotifier{}.New())
-	//notifiers = append(notifiers, AjouChembioNotifier{}.New())
-	//notifiers = append(notifiers, AjouCheNotifier{}.New())
-	//notifiers = append(notifiers, AjouMSENotifier{}.New())
-	//notifiers = append(notifiers, AjouEnvNotifier{}.New())
-	//notifiers = append(notifiers, AjouCENotifier{}.New())
-	//notifiers = append(notifiers, AjouTSENotifier{}.New())
-	//notifiers = append(notifiers, AjouArchNotifier{}.New())
-	//notifiers = append(notifiers, AjouISENotifier{}.New())
-	//notifiers = append(notifiers, AjouMobilityNotifier{}.New())
-	//notifiers = append(notifiers, AjouBizNotifier{}.New())
-	//notifiers = append(notifiers, AjouAbizNotifier{}.New())
-	//notifiers = append(notifiers, AjouEBizNotifier{}.New())
-	//notifiers = append(notifiers, AjouFENotifier{}.New())
-	//notifiers = append(notifiers, AjouGBNotifier{}.New())
-	//notifiers = append(notifiers, AjouNSNotifier{}.New())
-	//notifiers = append(notifiers, AjouMathNotifier{}.New())
-	//notifiers = append(notifiers, AjouPhysicsNotifier{}.New())
-	//notifiers = append(notifiers, AjouChemNotifier{}.New())
-	//notifiers = append(notifiers, AjouBiologyNotifier{}.New())
-	//notifiers = append(notifiers, AjouCossNotifier{}.New())
-	//notifiers = append(notifiers, AjouEconomicsNotifier{}.New())
-	//notifiers = append(notifiers, AjouPbaNotifier{}.New())
-	//notifiers = append(notifiers, AjouPsychologyNotifier{}.New())
-	//notifiers = append(notifiers, AjouSociologyNotifier{}.New())
-	//notifiers = append(notifiers, AjouPolNotifier{}.New())
-	//notifiers = append(notifiers, AjouSportsLeisureNotifier{}.New())
-	//notifiers = append(notifiers, AjouHumanitiesNotifier{}.New())
-	//notifiers = append(notifiers, AjouKoreanNotifier{}.New())
-	//notifiers = append(notifiers, AjouEnglishNotifier{}.New())
-	//notifiers = append(notifiers, AjouFrenchNotifier{}.New())
-	//notifiers = append(notifiers, AjouHistoryNotifier{}.New())
-	//notifiers = append(notifiers, AjouCultureContentsNotifier{}.New())
-	//notifiers = append(notifiers, AjouDasanNotifier{}.New())
-	//notifiers = append(notifiers, AjouInternationalNotifier{}.New())
-	//notifiers = append(notifiers, AjouGraduateNotifier{}.New())
 
 	noticeTicker := time.NewTicker(CrawlingPeriod * time.Second)
 	defer noticeTicker.Stop()
--- src/utils/utils.go
+++ src/utils/utils.go
@@ -1,9 +1,12 @@
 package utils
 
 import (
-	"Notifier/src/models"
+	. "Notifier/models"
 	"cloud.google.com/go/firestore"
+	"context"
 	"encoding/json"
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
 	"log"
 	"os"
 )
@@ -13,43 +16,58 @@ const (
 	CrawlingPeriod    = 10
 )
 
-var BoxCountMaxNumLogger *log.Logger
 var ErrorLogger *log.Logger
 var SentNoticeLogger *log.Logger
 var Client *firestore.Client
 
-func CreateDir(dir string) {
-	_, err := os.Stat(dir)
+func CreateDir(path string) {
+	_, err := os.Stat(path)
 	if os.IsNotExist(err) {
-		err = os.Mkdir("logs", os.ModePerm)
+		err = os.Mkdir(path, os.ModePerm)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
 }
 
-func LoadConfig(fileAddress string) ([]models.NotifierConfig, error) {
-	var manifests []models.NotifierConfig
+func OpenLogFile(path string) *os.File {
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0700)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return file
+}
+
+func CreateLogger(file *os.File) *log.Logger {
+	return log.New(file, "", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func ConnectFirebase() *firestore.Client {
+	sa := option.WithCredentialsFile("serviceAccountKey.json")
+	app, err := firebase.NewApp(context.Background(), nil, sa)
+	if err != nil {
+		log.Fatal(err)
+	}
+	Client, err = app.Firestore(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	return Client
+}
+
+func LoadConfig(fileAddress string) []NotifierConfig {
 	file, err := os.Open(fileAddress)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
+
+	var configs []NotifierConfig
 	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&manifests)
+	err = decoder.Decode(&configs)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return manifests, err
-}
 
-func CreateSelectorMap() {
-	selectorMap := make(map[int]map[string]interface{})
-	selectorMap[1] = map[string]interface{}{
-		"": "",
-	}
-	selectorMap[2] = map[string]interface{}{
-		"box": "#cms-content > div > div > div.bn-list-common02.type01.bn-common-cate > table > tbody > tr[class$=\"b-top-box\"]",
-		"num": "#cms-content > div > div > div.bn-list-common02.type01.bn-common-cate > table > tbody > tr:not([class$=\"b-top-box\"])",
-	}
+	return configs
 }
