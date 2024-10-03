9abe8985380275103809d633b2115883eb566c42
심재엽 <jysim0326@ajou.ac.kr>
2024-10-03 20:27:48 +0900

    [Refactor] Webhook시 토큰 활용

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -1,147 +1,194 @@
 package utils
 
 import (
-	"bytes"
-	"database/sql"
-	"encoding/json"
-	"io"
-	"log"
-	"net/http"
-	"os"
-
-	. "Notifier/models"
-	"github.com/PuerkitoBio/goquery"
-	"github.com/go-sql-driver/mysql"
+    "bytes"
+    "context"
+    "database/sql"
+    "encoding/json"
+    "io"
+    "log"
+    "net/http"
+    "github.com/go-redis/redis/v8"
+    "os"
+    "fmt"
+    . "Notifier/models"
+    "github.com/PuerkitoBio/goquery"
+    "github.com/go-sql-driver/mysql"
 )
 
 var ErrorLogger *log.Logger
 var SentNoticeLogger *log.Logger
 var PostLogger *log.Logger
 var DB *sql.DB
+var ctx = context.Background()
 
 func CreateDir(path string) {
-	_, err := os.Stat(path)
-	if os.IsNotExist(err) {
-		err = os.Mkdir(path, os.ModePerm)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
+    _, err := os.Stat(path)
+    if os.IsNotExist(err) {
+        err = os.Mkdir(path, os.ModePerm)
+        if err != nil {
+            log.Fatal(err)
+        }
+    }
+}
+
+// Redis에서 crawling-token 가져오기
+func GetTokenFromRedis() string {
+    // Redis 클라이언트 설정
+    redisHost := os.Getenv("REDIS_HOST")
+    redisPort := os.Getenv("REDIS_PORT")
+
+    rdb := redis.NewClient(&redis.Options{
+        Addr: redisHost + ":" + redisPort, // 환경변수에서 Redis 호스트 정보 가져오기
+    })
+
+    // crawling-token 키로 Redis에서 토큰 가져오기
+    token, err := rdb.Get(ctx, "crawling-token").Result()
+    if err != nil {
+        log.Fatalf("Failed to get token from Redis: %v", err)
+    }
+    return token
 }
 
 func OpenLogFile(path string) *os.File {
-	file, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0777)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return file
+    file, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0777)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return file
 }
 
 func CreateLogger(file *os.File) *log.Logger {
-	return log.New(file, "", log.Ldate|log.Ltime|log.Lshortfile)
+    return log.New(file, "", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 func ConnectDB() *sql.DB {
-	config := mysql.Config{
-		User:                 os.Getenv("DB_USER"),
-		Passwd:               os.Getenv("DB_PW"),
-		Net:                  "tcp",
-		Addr:                 os.Getenv("DB_IP") + ":" + os.Getenv("DB_PORT"),
-		DBName:               os.Getenv("DB_NAME"),
-		AllowNativePasswords: true,
-	}
-	connector, err := mysql.NewConnector(&config)
-	if err != nil {
-		ErrorLogger.Panic(err)
-	}
-
-	db := sql.OpenDB(connector)
-	err = db.Ping()
-	if err != nil {
-		ErrorLogger.Panic(err)
-	}
-
-	return db
+    config := mysql.Config{
+        User:                 os.Getenv("DB_USER"),
+        Passwd:               os.Getenv("DB_PW"),
+        Net:                  "tcp",
+        Addr:                 os.Getenv("DB_IP") + ":" + os.Getenv("DB_PORT"),
+        DBName:               os.Getenv("DB_NAME"),
+        AllowNativePasswords: true,
+    }
+    connector, err := mysql.NewConnector(&config)
+    if err != nil {
+        ErrorLogger.Panic(err)
+    }
+    db := sql.OpenDB(connector)
+    err = db.Ping()
+    if err != nil {
+        ErrorLogger.Panic(err)
+    }
+    return db
 }
 
 func LoadNotifierConfig(path string) []NotifierConfig {
-	file, err := os.Open(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	var configs []NotifierConfig
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&configs)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return configs
+    file, err := os.Open(path)
+    if err != nil {
+            log.Fatal(err)
+    }
+    defer file.Close()
+    var configs []NotifierConfig
+    decoder := json.NewDecoder(file)
+    err = decoder.Decode(&configs)
+    if err != nil {
+            log.Fatal(err)
+    }
+    return configs
 }
 
 func LoadDbData(topic string) (int, int) {
-	var boxCount int
-	query := "SELECT n.value FROM notice AS n JOIN topic AS t ON n.topic_id = t.id WHERE t.department = ? AND n.type = ?"
+    var boxCount int
+    query := "SELECT n.value FROM notice AS n JOIN topic AS t ON n.topic_id = t.id WHERE t.department = ? AND n.type = ?"
 
-	err := DB.QueryRow(query, topic, "box").Scan(&boxCount)
-	if err != nil {
-		log.Fatal(err)
-	}
+    err := DB.QueryRow(query, topic, "box").Scan(&boxCount)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	var maxNum int
-	err = DB.QueryRow(query, topic, "num").Scan(&maxNum)
-	if err != nil {
-		log.Fatal(err)
-	}
+    var maxNum int
+    err = DB.QueryRow(query, topic, "num").Scan(&maxNum)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	return boxCount, maxNum
+    return boxCount, maxNum
 }
 
+// 웹훅을 호출할 때 Redis에서 가져온 토큰을 Bearer로 헤더에 추가
 func SendCrawlingWebhook(url string, payload any) {
-	payloadJson, err := json.Marshal(payload)
-	if err != nil {
-		ErrorLogger.Panic(err)
-	}
+    payloadJson, err := json.Marshal(payload)
+    if err != nil {
+        ErrorLogger.Panic(err)
+    }
 	buff := bytes.NewBuffer(payloadJson)
 
-	resp, err := http.Post(url, "application/json", buff)
-	if err != nil {
-		ErrorLogger.Panic(err)
-	}
-	defer resp.Body.Close()
+    // Redis에서 crawling-token 가져오기
+    token := GetTokenFromRedis()
 
-	body, err := io.ReadAll(resp.Body)
+    // HTTP 요청 생성
+    req, err := http.NewRequest("POST", url, buff)
+    if err != nil {
+        ErrorLogger.Panic(err)
+    }
+
+    // Content-Type 헤더 설정
+    req.Header.Set("Content-Type", "application/json")
+
+	// Authorization 헤더에 Bearer 토큰 설정
+	req.Header.Set("Authorization", token)
+
+
+    // HTTP 클라이언트로 요청 보내기
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        ErrorLogger.Panic(err)
+    }
+    defer resp.Body.Close()
+
+    // 응답 본문 읽기
+    body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		ErrorLogger.Panic(err)
-	}
-	PostLogger.Println(string(body))
+        ErrorLogger.Panic(err)
+    }
+    PostLogger.Println(string(body))
 }
 
 func GetNumNoticeCountReference(doc *goquery.Document, englishTopic, boxNoticeSelector string) int {
-	if englishTopic != "Software" {
-		return 10
-	}
-	boxNoticeSels := doc.Find(boxNoticeSelector)
-	boxCount := boxNoticeSels.Length()
-	return 15 - boxCount
+    if englishTopic != "Software" {
+        return 10
+    }
+    boxNoticeSels := doc.Find(boxNoticeSelector)
+    boxCount := boxNoticeSels.Length()
+    return 15 - boxCount
 }
 
-func NewDocumentFromPage(url string) *goquery.Document {
-	resp, err := http.Get(url)
-	if err != nil {
-		ErrorLogger.Panic(err)
-	}
-	if resp.StatusCode != http.StatusOK {
-		ErrorLogger.Panicf("status code error: %s, url: %s", resp.Status, url)
-	}
-	defer resp.Body.Close()
+func NewDocumentFromPage(url string) (*goquery.Document, error) {
+    // HTTP GET 요청
+    resp, err := http.Get(url)
+    if err != nil {
+        // 네트워크 에러 발생 시 에러 반환
+        ErrorLogger.Printf("Network error: %s", err)
+        return nil, err
+    }
+    defer resp.Body.Close()
 
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		ErrorLogger.Panic(err)
-	}
+    // 응답 상태 코드 확인
+    if resp.StatusCode != http.StatusOK {
+        // 상태 코드 에러 발생 시 에러 반환
+        ErrorLogger.Printf("Status code error: %d, URL: %s", resp.StatusCode, url)
+        return nil, fmt.Errorf("status code error: %d, URL: %s", resp.StatusCode, url)
+    }
 
-	return doc
+    // HTML 문서 파싱
+    doc, err := goquery.NewDocumentFromReader(resp.Body)
+    if err != nil {
+        // 파싱 에러 발생 시 에러 반환
+        ErrorLogger.Printf("Error parsing document: %s", err)
+        return nil, err
+    }
+
+    return doc, nil
 }
